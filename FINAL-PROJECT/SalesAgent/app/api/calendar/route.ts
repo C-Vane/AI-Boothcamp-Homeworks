527496a09b2f5cf1c7d0a0ccd1f00801f81e854d
vaneCat <vanebrunocattabiani@gmail.com>
2024-12-17 17:14:16 +0700

    feat: coment next tasks

---
+++ FINAL-PROJECT/SalesAgent/app/api/calendar/route.ts
@@ -0,0 +1,35 @@
+// GET USER Available in this week
+// owner email address
+// {
+//     "email": "example@email.com"
+// }
+
+// return date and time of each availability
+// [
+//     {
+//         "date": "2023-06-01",
+//         "time": "09:00:00"
+//     },
+//     {
+//         "date": "2023-06-01",
+//         "time": "10:00:00"
+//     }
+// ]
+// Get Availability
+export async function GET() {}
+
+// date and time of each availability
+// [
+//     {
+//         "date": "2023-06-01",
+//         "startTime": "09:00:00",
+//         "endTime": "10:00:00",
+//         "title": "Meeting with John",
+//         "targetEmail": "example@email.com"
+//         "owener": "example@email.com"
+//     },
+// ]
+//
+//Return true
+// Create event
+export async function POST() {}
